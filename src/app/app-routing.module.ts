d0a2bb4fe1ccf96c65f45fcc47ddb5ad4a3e0b91
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-04-10 11:20:34 +0200

    aggiunto welcome-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'login', component: LoginComponent},
+  {path: 'welcome/:userId', component: WelcomeComponent}
 ];
 
 @NgModule({
