76d628ce439f0903e440b4e61d318b5bcb8254e5
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-04-10 11:33:16 +0200

    aggiunto error-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ArticoliComponent } from './articoli/articoli.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     WelcomeComponent,
-    ArticoliComponent
+    ArticoliComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
