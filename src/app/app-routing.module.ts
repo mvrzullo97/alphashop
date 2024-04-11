7a085d88bbc41dd85c6e69e80706bd4d88ab0cb1
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-04-11 16:02:28 +0200

    aggiunta route-guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ArticoliComponent } from './articoli/articoli.component';
 import { ErrorComponent } from './error/error.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from 'src/services/route-guard.service';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'login', component: LoginComponent},
-  {path: 'welcome/:userId', component: WelcomeComponent},
-  {path: 'articoli', component: ArticoliComponent},
+  /* aggiungo controlli per verificare che l'utente sia loggato correttamente prima di accedere alla pagina in questione */
+  {path: 'welcome/:userId', component: WelcomeComponent, canActivate:[RouteGuardService]},
+  {path: 'articoli', component: ArticoliComponent, canActivate:[RouteGuardService]},
   {path: 'logout', component: LogoutComponent},
   
   {path:'**', component: ErrorComponent}
